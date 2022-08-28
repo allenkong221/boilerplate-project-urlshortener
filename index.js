45586624526ad146add7bbb8b434708110774d1e
allenkong221 <allen.kong@uconn.edu>
2022-08-27 22:53:03 -0400

    updated index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+let routes = [];
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +20,24 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function (req, res) {
+  const postUrl = req.body.url;
+  if(postUrl && /^(ftp|http|https):\/\/[^ "]+$/.test(postUrl)) {
+    routes.push(postUrl);
+    res.json({
+      original_url: postUrl,
+      short_url: routes.length
+    });
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+app.get('/api/shorturl/:index', function (req, res) {
+  const target = routes[req.params.index - 1];
+  target ? res.redirect(target) : res.json({error: 'invalid id'});
 });
 
 app.listen(port, function() {
